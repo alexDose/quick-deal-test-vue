2687e3f4555229665705686fadbed8c82d23ca22
Alexei <i0ver@mail.ru>
2023-07-07 20:32:48 +0300

    ref

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
 import { ITaskProps } from "@/types/Task";
-const TASK_STORAGE_KEY = "tasks";
+const TASKS_STORAGE_KEY = "tasks";
 
 const store = createStore({
   state: {
@@ -12,18 +12,18 @@ const store = createStore({
     },
     addTask(state, task: ITaskProps) {
       state.tasks.push(task);
-      localStorage.setItem(TASK_STORAGE_KEY, JSON.stringify(state.tasks));
+      localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(state.tasks));
     },
     updateTask(state, updatedTask: ITaskProps) {
       const task = state.tasks.find((task) => task.id === updatedTask.id);
       if (task) {
         task.name = updatedTask.name;
       }
-      localStorage.setItem(TASK_STORAGE_KEY, JSON.stringify(state.tasks));
+      localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(state.tasks));
     },
     deleteTask(state, taskId: number) {
       state.tasks = state.tasks.filter((task) => task.id !== taskId);
-      localStorage.setItem(TASK_STORAGE_KEY, JSON.stringify(state.tasks));
+      localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(state.tasks));
     },
   },
   actions: {
@@ -39,7 +39,7 @@ const store = createStore({
   },
 });
 
-const tasksData = localStorage.getItem(TASK_STORAGE_KEY);
+const tasksData = localStorage.getItem(TASKS_STORAGE_KEY);
 if (tasksData != null) {
   store.commit("setTasks", JSON.parse(tasksData));
 }
